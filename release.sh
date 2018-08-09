d65bb17c355e1f2ee03cf79c9d3b1ec1b5a20f99
Kehinde Shittu <kehesjay@gmail.com>
2018-08-09 18:37:03 +0100

    Updates readme and release.sh

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o nounset -o xtrace -o errexit -o pipefail
+
+PAYLOAD="{
+    \"build_parameters\": {
+        \"MVN_RELEASE_TAG\": \"v${DWAPISPEC_NOW_VERSION}\",
+        \"MVN_RELEASE_VER\": \"${DWAPISPEC_NOW_VERSION}\",
+        \"MVN_RELEASE_DEV_VER\": \"${DWAPISPEC_NEXT_VERSION}-SNAPSHOT\",
+        \"MVN_RELEASE_USER_EMAIL\": \"${DWAPISPEC_RELEASE_USER_EMAIL}\",
+        \"MVN_RELEASE_USER_NAME\": \"Via CircleCI\"
+    }
+}"
+
+#curl -v -X POST -H "Content-Type: application/json" -d "${PAYLOAD}" "https://circleci.com/api/v1/project/datadotworld/dwapi-spec/tree/master?circle-token=${CIRCLE_TOKEN}"
